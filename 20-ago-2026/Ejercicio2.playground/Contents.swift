b48f59a1c6e6b9005dccad16f9bb99430eb91ed6
MacBook 6 <macbook6@MacBook-6.local>
2026-08-20 14:45:56 -0600

    20-ago-2026

---
+++ 20-ago-2026/Ejercicio2.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+let windth = 10
+let height = 20
+let area = windth * height
+print (area)
+
+let roomArea = area / 2
+
+print (roomArea)
+
+let perimeter = windth * 2 + height * 2
+print (perimeter)
