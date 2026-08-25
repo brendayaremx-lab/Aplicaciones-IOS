4809afdc19c1077748c8b0eb2bf0fd0a5fcd97c6
MacBook 6 <macbook6@MacBook-6.local>
2026-08-25 14:20:55 -0600

    Ejercicio 2

---
+++ 25-ago-2026/Ejercicio2.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+let name = "Brenda Cardoso"
+
+let favorite = "My favorite quote is \"I am Iron Man\""
+
+let emptyString = ""
+
+if emptyString.isEmpty {
+    print("Theres nothing here")
+}else{
+    print("Its not as empty as I thought")
+}
